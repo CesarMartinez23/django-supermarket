be6f5e8f1622c75653b80c64ee2659fe4a41c5da
CesarMartinez23 <oliver-cesar@hotmail.com>
2022-11-12 21:29:56 -0600

    Estructura de paginas

--- Products/urls.py
+++ Products/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('products/', views.products),
-    path('contact/', views.contact),
+    path('', views.index, name="home"),
+    path('products/', views.products, name="products"),
+    path('contact/', views.contact, name="contact"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('about/', views.about, name="about"),
 ]
--- Products/views.py
+++ Products/views.py
@@ -7,7 +7,16 @@ def index(request):
     return render(request, 'index.html')
 
 def products(request):
-    return render(request, 'products.html')
+    return render(request, 'pages/products.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'pages/contact.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def register(request):
+    return render(request, 'pages/register.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
